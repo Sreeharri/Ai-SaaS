741ca88f78d11aa8491da5c549ebbf27840a1d22
Sreeharri <sreeharri2001@gmail.com>
2025-07-28 20:41:14 +0200

    just in middle of dashborad bro

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import { Menu, X } from 'lucide-react'
+import Sidebar from '../components/Sidebar'
+import {SignIn, useUser } from '@clerk/clerk-react'
 
 const Layout = () => {
-  return (
-    <div>
-      <h1>Layout</h1>
-      <Outlet /> {/* This will render the nested routes */}
+
+  const navigate = useNavigate();
+  const [sidebar , setSidebar] = React.useState(false);
+  const {user} = useUser();
+
+  return user ? (
+    <div className='flex flex-col items-start justify-start h-screen'>
+      <nav className='w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200'>
+        <img className='cursor-pointer' onClick={() => navigate('/')} src={assets.logo} alt="" />
+        {
+            sidebar ? < X onClick={() => setSidebar(false)}  className='w-6 h-6 text-gray-600 sm:hidden'/> : < Menu onClick={() => setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden'/>
+        }
+      </nav>
+      <div className='flex-1 w-full flex h-[calc(100vh-64px)]'>
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
+        <div className='flex-1 bg-[#F4F7FB]'>
+          <Outlet />
+        </div>
+
+      </div>
+      
+      
+    </div>
+  ) : (
+    <div className='flex items-center justify-center h-screen' >
+      <SignIn />
     </div>
   )
+   
 }
 
 export default Layout
